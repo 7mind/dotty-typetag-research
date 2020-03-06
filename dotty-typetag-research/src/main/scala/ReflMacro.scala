8bd05ae137a7ae5b1ff6792cdd766d708774fca2
=Ivan Kamyshan <ivan71pml27@gmail.com>
2020-03-06 17:14:33 +0200

    Typetags first attempt.

--- dotty-typetag-research/src/main/scala/ReflMacro.scala
+++ dotty-typetag-research/src/main/scala/ReflMacro.scala
@@ -2,12 +2,13 @@ import scala.deriving._
 import scala.quoted._
 import scala.quoted.matching._
 import scala.compiletime.{erasedValue, summonFrom, constValue}
+import LightTypeTagRefPkg._
 
 object Inspect {
-  inline def inspect[T]: String = ${ labelImpl[T] }
-  def labelImpl[T: Type](given qctx: QuoteContext): Expr[String] = {
-    InspectMacro.apply[T]
-    Expr("")
+  inline def inspect[T]: AbstractReference = ${ labelImpl[T] }
+  def labelImpl[T: Type](given qctx: QuoteContext): Expr[AbstractReference] = {
+    println(InspectMacro.apply[T])
+    Expr(null)
   }
 }
 
@@ -16,89 +17,125 @@ abstract class Inspector(shift: Int) { self =>
   given as qctx.type = qctx
   import qctx.tasty.{Type => TType, given _, _}
 
-  def inspect[T](tpe: Type[T]): Unit = {
+  def inspect[T](tpe: Type[T]): AbstractReference = {
       val uns = tpe.unseal
-      inspectTree(uns)
+      val v = inspectTree(uns)
       println("--------")
+      v
   }
 
   private def logStart(s: String) = println(" " * shift + s)
   private def log(s: String) = println(" " * shift + " -> " + s)
   private def next() = new Inspector(shift + 1) { val qctx: self.qctx.type = self.qctx }
-
-  private def inspectToB(tpe: TypeOrBounds): Unit = {
+  private def inspectToB(tpe: TypeOrBounds): TypeParam = {
     tpe match {
       case t: TypeBounds =>
         ???
       case t: TType =>
-        inspectTType(t)
+        TypeParam(inspectTType(t), Variance.Invariant)
     }
   }
 
-  private def inspectTType(tpe2: TType): Unit = {
+  private def flattenInspectAnd(and: AndType): Set[AppliedReference] = {
+    val (andTypes, otherTypes) =
+    and match
+      case AndType(l @ AndType(_,_), r  @ AndType(_,_)) => (Set(l,r),Set.empty[TType])
+      case AndType(l @ AndType(_,_), r) =>  (Set(l),Set(r))
+      case AndType(l, r  @ AndType(_,_)) => (Set(r),Set(l))
+      case AndType(l, r) => (Set.empty[AndType], Set(l,r))
+    val andTypeTags = andTypes flatMap flattenInspectAnd
+    val otherTypeTags = otherTypes map inspectTType map {_.asInstanceOf[AppliedReference]}
+    andTypeTags ++ otherTypeTags
+  }
+
+  private def flattenInspectOr(or: OrType): Set[AppliedReference] = {
+    val (orTypes, otherTypes) =
+    or match
+      case OrType(l @ OrType(_,_), r  @ OrType(_,_)) => (Set(l,r),Set.empty[TType])
+      case OrType(l @ OrType(_,_), r) =>  (Set(l),Set(r))
+      case OrType(l, r  @ OrType(_,_)) => (Set(r),Set(l))
+      case OrType(l, r) => (Set.empty[OrType], Set(l,r))
+    val orTypeTags = orTypes flatMap flattenInspectOr
+    val otherTypeTags = otherTypes map inspectTType map {_.asInstanceOf[AppliedReference]}
+    orTypeTags ++ otherTypeTags
+  }
+
+  private def inspectTType(tpe2: TType): AbstractReference = {
     tpe2 match {
       case a: AppliedType =>
         log(s"APPLIED: ${a.tycon};; ${a.args}")
+        val args = a.args.map{x => next().inspectToB(x)}
+        FullReference(a.tycon.toString, args)
         next().inspectSymbol(a.tycon.typeSymbol)
-        a.args.map {
-          a =>
-            next().inspectToB(a)
-        }
+
       case l: TypeLambda =>
         log(s"LAMBDA: ${l.paramNames} ${l.resType}")
-        next().inspectTType(l.resType)
+        val resType = next().inspectTType(l.resType)
+        val paramNames = l.paramNames.map{LambdaParameter(_)}
+        LightTypeTagRefPkg.Lambda(paramNames, resType)
+
       case t: ParamRef =>
         log(s"PARAM REF: $t ")
+        NameReference(t.toString)
+      case a: AndType =>
+        log(s"AND: rhs[${a.left}], lhs[${a.right}]")
+        IntersectionReference(flattenInspectAnd(a))
+
+      case o: OrType =>
+        log(s"OR: rhs[${o.left}], lhs[${o.right}]")
+        UnionReference(flattenInspectOr(o))
+
       case r: TypeRef =>
         //log(s"TYPEREF: ${r.name} ${r.translucentSuperType}")
         log(s"TYPEREF: ${r.name} ${r.typeSymbol}")
         next().inspectSymbol(r.typeSymbol)
+
       case o =>
         log(s"TTYPE, UNSUPPORTED: $o")
+        ???
     }
   }
 
-  private def inspectTree(uns: TypeTree): Unit = {
+  private def inspectTree(uns: TypeTree): AbstractReference = {
       val symbol = uns.symbol
       val tpe2 = uns.tpe
       logStart(s"INSPECT: $uns: ${uns.getClass}")
-
       // println(s"TPE: ${tpe}: ${tpe.getClass}\n  * UNSEALED: ${uns}: ${uns.getClass}")
       // println(s"SYMBOL: ${symbol}: ${symbol.getClass}")
       // println(s"SYMBOL: ${symbol.show}")
       // println(s"TREETPE: ${tpe2}: ${tpe2.getClass}")
       // println(s"TREETPE: ${tpe2.show}")
-
-      if (symbol.isNoSymbol) {
+      if (symbol.isNoSymbol)
         inspectTType(tpe2)
-      } else {
+      else
         inspectSymbol(symbol)
-      }
   }
-  private def inspectSymbol(symbol: Symbol) = {
-    log(s"SYMTPE: ${symbol}; ${symbol.flags.is(Flags.Covariant)}")
+
+  private def inspectSymbol(symbol: Symbol): AbstractReference = {
+    log(s"SYMTPE: ${symbol}; covariant:${symbol.flags.is(Flags.Covariant)}")
     symbol.tree match {
       case c: ClassDef =>
         log(s"CLASSDEF, parents: ${c.parents}")
-        c.parents.map {
-          a =>
-            next().inspectTree(a.asInstanceOf[TypeTree])
-        }
+        // c.parents.map {
+        //   a =>
+        //     next().inspectTree(a.asInstanceOf[TypeTree])
+        // }
+        FullReference(c.name, Nil)
       case t: TypeDef =>
         log(s"TYPEDEF: $t")
         next().inspectTree(t.rhs.asInstanceOf[TypeTree])
       case o =>
         log(s"SYMBOL, UNSUPPORTED: $o")
+        ???
     }
   }
 
   private def tpeAttrs[T](uns: TypeTree): Unit = {
       val symbol = uns.symbol
-
       println(s"Attrs[$uns]: type=${symbol.isType}, term=${symbol.isTerm}, packageDef=${symbol.isPackageDef}, classDef=${symbol.isClassDef}, typeDef=${symbol.isValDef}, defdef=${symbol.isDefDef}, bind=${symbol.isBind}, nosymbol=${symbol.isNoSymbol}")
   }
 }
 
 object InspectMacro {
-  def apply[T](given qctx0: QuoteContext, tpe: Type[T]): Unit = new Inspector(0) { val qctx = qctx0 }.inspect(tpe)
+  def apply[T](given qctx0: QuoteContext, tpe: Type[T]): AbstractReference = new Inspector(0) { val qctx = qctx0 }.inspect(tpe)
 }
